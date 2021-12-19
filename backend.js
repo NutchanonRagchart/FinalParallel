61cbd749acfbd7309a7fc469a37c17c66db1a0d2
Nutchanon Ragchart <nutchanon.rag@student.mahidol.edu>
2021-12-19 16:17:58 +0700

    test edit

--- backend.js
+++ backend.js
@@ -56,25 +56,33 @@ router.get('/allproducts/:keyword', cors(), function (req, res) {
     console.log("search for = " + req.params.keyword);
     let word = req.params.keyword;
     let result;
-    if (word != "") {
-        const request = new Request(
-            `SELECT * FROM Product WHERE Productname LIKE '%${word}%'`, (err, rowCount) => {
-                if (err) {
-                    console.error(err.message);
-                }
-                else {
-                    console.log(`${rowCount} row(s) returned`);
-                    console.log(data);
-                    result = JSON.stringify(data);
-                    console.log(result)
-                    console.log(Bigdata)
-                    // Bigdata = JSON.stringify(Bigdata)
-                    // sendR(result);
-                    return res.send({ error: false, data: Bigdata, message: 'Result of rooms444' });
-                }
-            }
-        );
-        connection.execSql(request);
+    if (!word) {
+        return res.status(400).send({ error: true, message: 'Please provide product name' });
+        
+    }
+    searchName(word);
+
+    function searchName(word){
+         const request = new Request(
+             SELECT * FROM Product WHERE Productname LIKE '%${word}%', 
+             (err, rowCount) => {
+                 if (err) {
+                     console.error(err.message);
+                 }else{
+                     console.log(err.message)
+                     console.log(${rowCount} row(s) returned);
+                     console.log(data);
+                     result = JSON.stringify(data);
+                     console.log(result)
+                     console.log(Bigdata)
+                     // Bigdata = JSON.stringify(Bigdata)
+                     // sendR(result);
+                     return res.send({ error: false, data: Bigdata, message: 'Result of rooms444' });
+                 }
+             }
+         );
+    
+    connection.execSql(request);
 
         var counter = 1;
         const data = {};
